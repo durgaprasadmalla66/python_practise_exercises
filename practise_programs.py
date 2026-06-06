4c9a2dda10cfc9f3e3a171da848242fba4370647
durgaprasadmalla66 <durgaprasadmalla66@gmail.com>
2026-06-06 07:47:08 +0530

    initial comit

---
+++ practise_programs.py
@@ -0,0 +1,14 @@
+word = "python"
+guess = ""
+guess_count = 0
+guess_limit = 5
+out_of_guesses = False
+while guess != word and not out_of_guesses:
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ").lower()
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("Out of guesses, you lose!")
+else:    print("You win!")        
