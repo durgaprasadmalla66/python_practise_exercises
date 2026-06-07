fd60570b806814e7e6ab97d34bb867b2aeaa3624
durgaprasadmalla66 <durgaprasadmalla66@gmail.com>
2026-06-07 08:20:43 +0530

    finding even numbers using if else

--- practise_programs.py
+++ practise_programs.py
@@ -13,3 +13,11 @@ while guess != word and not out_of_guesses:
 if out_of_guesses:
     print("Out of guesses, you lose!")
 else:    print("You win!")        
+
+
+#finding even numbers using if-else statement
+x= int(input("Enter a number: "))
+if x % 2 == 0:
+    print(f"{x} is an even number.")
+else:
+    print(f"{x} is an odd number.")
